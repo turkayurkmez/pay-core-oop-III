6197f96de41548a5d8106c6b89e60fddc1f4bf5f
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-07-11 21:39:19 +0300

    Adding strategy pattern

---
+++ DesignPatterns/Strategy/DP.cs
@@ -0,0 +1,33 @@
+﻿namespace Strategy
+{
+
+    public interface ISortAlgorithm
+    {
+        void Sort();
+
+    }
+    public class ProductCollection
+    {
+        public void Sort(ISortAlgorithm sortAlgorithm)
+        {
+            //BubbleSort....
+            sortAlgorithm.Sort();
+        }
+    }
+
+    public class BubbleSortAlgorithm : ISortAlgorithm
+    {
+        public void Sort()
+        {
+            Console.WriteLine("Bubble");
+        }
+    }
+
+    public class HeapAlgorithm : ISortAlgorithm
+    {
+        public void Sort()
+        {
+            Console.WriteLine("Heap");
+        }
+    }
+}
---
+++ DesignPatterns/Strategy/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Strategy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductCollection productCollection = new ProductCollection();
+            BubbleSortAlgorithm bubbleSortAlgorithm = new BubbleSortAlgorithm();
+            HeapAlgorithm heapAlgorithm = new HeapAlgorithm();
+            productCollection.Sort(bubbleSortAlgorithm);
+            productCollection.Sort(heapAlgorithm);
+        }
+    }
+}
